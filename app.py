c0d6cc06e48c9f3bac7d352314eaa90620b25676
Alisha <jafrya04@gmail.com>
2024-04-17 01:07:36 -0400

    set up streamlit app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import requests
+from datetime import datetime
+
+# api_key="e1917edb-7526-4253-bcaf-08e76d1f15f9"
+
+
+st.title("Art Web App")
+st.header("Art Institute of Chicago")
+
