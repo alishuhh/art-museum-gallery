503290be709d04c80ca6c444d86cacb3f9dc0106
Ni-Val <122932066+Ni-Val@users.noreply.github.com>
2024-04-22 15:29:10 -0400

    added map of the MET

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import os
+import os
 import streamlit as st
 import requests
 import pandas as pd
@@ -7,6 +8,7 @@ import numpy as np
 css_file = os.path.abspath('style.css')
 with open(css_file) as css:
     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
 
 # Function to fetch artworks from the API
 def fetch_artworks(url):
@@ -19,15 +21,18 @@ def fetch_artworks(url):
         st.error(f'Failed to fetch data: {response.status_code}')
         return None
 
+
 # Function to fetch a single artwork by ID
 def fetch_artwork_by_id(artwork_id):
     artwork_url = f"https://api.artic.edu/api/v1/artworks/{artwork_id}"
     return fetch_artworks(artwork_url)
 
+
 # Function to search artworks
 def search_artworks(query):
     search_url = f"https://api.artic.edu/api/v1/artworks/search?q={query}"
     return fetch_artworks(search_url)
+
 
 # Main app logic
 st.sidebar.header("Search The Collection")
@@ -58,25 +63,30 @@ artist_selection = st.sidebar.selectbox("Artist", artist_choices)
 
 # Additional choices for departments
 department_choices = ["", "Applied Arts of Europe", "Architecture and Design", "Arts of Africa", "Arts of Asia",
-                       "Arts of the Americas", "Arts of the Ancient Mediterranean and Byzantium", "Contemporary Art",
-                       "Modern Art", "Modern and Contemporary Art", "Painting and Sculpture of Europe",
-                       "Photography and Media", "Prints and Drawings", "Research Center", "Textiles"]
+                      "Arts of the Americas", "Arts of the Ancient Mediterranean and Byzantium", "Contemporary Art",
+                      "Modern Art", "Modern and Contemporary Art", "Painting and Sculpture of Europe",
+                      "Photography and Media", "Prints and Drawings", "Research Center", "Textiles"]
 department_selection = st.sidebar.selectbox("Department", department_choices)
 
 # Checkboxes for "On view"
 show_only_on_view = st.sidebar.checkbox("Show Pieces On View")
 
-
 st.title("Art Museum Gallery")
 st.header("Explore your favorite museum's collection")
 
-museum_choice = st.selectbox("Select a museum", ["", "Art Institute of Chicago"])
+museum_choice = st.selectbox("Select a museum", ["", "Art Institute of Chicago", "The Metropolitan Museum of Arts"])
 
 if museum_choice == "Art Institute of Chicago":
     st.subheader("Location")
     # Coordinates of the Art Institute of Chicago
     latitude = 41.8796
     longitude = -87.6224
+elif museum_choice == "The Metropolitan Museum of Arts":
+    st.subheader("Location")
+    latitude = 40.779434
+    longitude = -73.963402
+
+
 
     # Create a DataFrame with the coordinates
     location = pd.DataFrame({'latitude': [latitude], 'longitude': [longitude]})
@@ -84,7 +94,6 @@ if museum_choice == "Art Institute of Chicago":
     # Display the map centered on the Art Institute of Chicago
     st.map(data=location, zoom=15)
 
-    
     if search_button:
         if search_query:
             search_results = search_artworks(search_query)
@@ -101,7 +110,7 @@ if museum_choice == "Art Institute of Chicago":
                         st.write(artwork_details.get('place_of_origin', ''))
                         st.write("Medium:", artwork_details.get('medium_display', ''))
                         st.write("------------")
-               
+
             else:
                 st.info("No artworks found. Please refine your search criteria.")
         else:
@@ -112,17 +121,17 @@ if museum_choice == "Art Institute of Chicago":
         if default_results is not None:
             default_df = pd.DataFrame(default_results)
             columns_to_save = ['artist_title', 'title', 'place_of_origin', 'medium_display',
-                                'category_titles','style_title', 'image_id', 'department_title', 'is_on_view']
+                               'category_titles', 'style_title', 'image_id', 'department_title', 'is_on_view']
             default_df[columns_to_save].to_csv('artworks.csv', index=False)
 
             for index, artwork in default_df.iterrows():
                 # Filter based on user selections
                 if (style_selection == "" or artwork['style_title'] == style_selection) and \
-                   (type_selection == "" or artwork['type'] == type_selection) and \
-                   (origin_selection == "" or artwork['place_of_origin'] == origin_selection) and \
-                   (artist_selection == "" or artwork['artist_title'] == artist_selection) and \
-                   (department_selection == "" or artwork['department_title'] == department_selection) and \
-                   (not show_only_on_view or artwork['is_on_view']):
+                        (type_selection == "" or artwork['type'] == type_selection) and \
+                        (origin_selection == "" or artwork['place_of_origin'] == origin_selection) and \
+                        (artist_selection == "" or artwork['artist_title'] == artist_selection) and \
+                        (department_selection == "" or artwork['department_title'] == department_selection) and \
+                        (not show_only_on_view or artwork['is_on_view']):
                     image_id = artwork['image_id']
                     image_url = f"https://www.artic.edu/iiif/2/{image_id}/full/400,/0/default.jpg"
                     st.image(image_url, width=200)
@@ -136,4 +145,5 @@ if museum_choice == "Art Institute of Chicago":
 
 
 else:
-    st.image(os.path.abspath('misc/Charles Van den Eycken.jpg'), width=700, caption="Kittens At Play By Charles Van Den Eycken")
+    st.image(os.path.abspath('misc/Charles Van den Eycken.jpg'), width=700,
+             caption="Kittens At Play By Charles Van Den Eycken")
